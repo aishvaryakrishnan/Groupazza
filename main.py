1b0d23497cc9f8285584bbce10cae191b9378261
Jon Khaykin <jon@jonkhaykin.com>
2015-08-08 17:08:18 -0700

    New changes

--- main.py
+++ main.py
@@ -39,7 +39,23 @@ def submit():
         new_user = { "user": user_info["email"], "p_user_id": user_info["user_id"], "classes": user_info["all_classes"]}
         result = fb.post('/users', new_user)
         user_id = new_user["p_user_id"]
-        return render_template('dashboard.html', result=new_user)
+        users = json.loads(json.dumps(fb.get('/users', None)))
+        u = ""
+        mlist = []
+        for user in users:
+            for u in users[user]:
+                if u == "classes":
+                  mlist.append(users[user][u])
+                 # class1 = p.network(mlist[0])
+        li = []
+        for index, item in enumerate(mlist):
+            if isinstance(item, dict):
+                for key in item:
+                    for i in item[key]:
+                        if "name" == i:
+                            li.append(item[key][i])
+        return render_template('dashboard.html', user=new_user, courses= li)
+        li = []
     if info:
        return render_template('chat.html', info=info)
     else:
